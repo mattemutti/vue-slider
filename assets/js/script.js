f56565787f554889972e596c60dfd74e8ba7a67f
mattemutti <mattemutti@gmail.com>
2024-03-12 18:29:16 +0100

    add bonus

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
 const { createApp } = Vue
 
 createApp({
-    data() { 
+    data() {
         return {
 
             activeImage: 0,
-            index: 0,
+            
 
             slides: [
                 {
@@ -17,38 +17,38 @@ createApp({
                     image: './assets/img/02.webp',
                     title: 'Ratchet & Clank: Rift Apart',
                     text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
-                }, 
+                },
                 {
                     image: './assets/img/03.webp',
                     title: 'Fortnite',
                     text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-                }, 
+                },
                 {
                     image: './assets/img/04.webp',
                     title: 'Stray',
                     text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-                }, 
+                },
                 {
                     image: './assets/img/05.webp',
                     title: "Marvel's Avengers",
                     text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
                 }
-            ]   
+            ]
 
         }
-            
-    }, 
-    
+
+    },
+
     methods: {
-        
+
         prev() {
-            this.activeImage--  
+            this.activeImage--
             //console.log(this.activeImage)
             //console.log(this.slides[0].image);
             if (this.activeImage < 0) {
                 this.activeImage = this.slides.length - 1;
             }
-           
+
         },
         next() {
             this.activeImage++
@@ -56,10 +56,23 @@ createApp({
             if (this.activeImage === this.slides.length) {
                 this.activeImage = 0;
             }
+        },
+
+        play() {
+            const play = setInterval(go, 3000);
         }
 
+    },
 
+    created() {
 
+        console.log('ciao belli', this);
+
+        function go() {
+            next();
+            console.log('passati 3 sec');
+        }
+        
     }
 
 
--- readme.md
+++ readme.md
@@ -10,4 +10,4 @@ Partendo dal markup della versione svolta in js plain, rifare lo slider ma quest
 Consigli del giorno:
 regola d'oro: riciclare ovunque possibile! Questo significa che per la parte di markup possiamo recuperare html e css dell'esercizio svolto qualche giorno fa: è già tutto pronto!
 
-il riciclo spesso va a braccetto con le funzioni! Sapendole sfruttare bene, l'esercizio si riduce a poche righe
+il riciclo spesso va a braccetto con le funzioni! Sapendole sfruttare bene, l'esercizio si riduce a poche righe 
